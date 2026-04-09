428a2eca9af99b7370ee674d835df64098033320
Votre Nom <votre@email.com>
2026-04-09 16:40:59 +0100

    Fix(updater): final GitHub action path and signature config

---
+++ gen_keys.ps1
@@ -0,0 +1,6 @@
+﻿$proc = Start-Process npx -ArgumentList "tauri signer generate" -PassThru -RedirectStandardOutput "new_keys.txt" -RedirectStandardError "new_keys_err.txt" -RedirectStandardInput "stdin.txt"
+Start-Sleep -Seconds 2
+Add-Content -Path "stdin.txt" -Value "`n"
+Start-Sleep -Seconds 1
+Add-Content -Path "stdin.txt" -Value "`n"
+Start-Sleep -Seconds 5
